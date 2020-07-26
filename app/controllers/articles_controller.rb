4599f64e39fadcf8f3adf7891615101e45ca43f8
James <jamesbaker425@gmail.com>
2020-07-26 13:47:16 -0400

    Update articles_controller.rb
    Added new, create, destroy, edit, and update functions.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,41 @@
 class ArticlesController < ApplicationController
+    include ArticlesHelper
+
+    #Homepage
     def index
         @articles = Article.all
     end
-    
+    #View specific article
     def show
         @article = Article.find(params[:id])
+    end
+    #New article form
+    def new
+        @article = Article.new
+    end
+    #New article submit
+    def create
+        @article = Article.new(article_params)
+        @article.save
+        flash.notice = "Article '#{@article.title}' Created!"
+        redirect_to article_path(@article)
+    end
+    #Delete article
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash.notice = "Article '#{@article.title}' Deleted!"
+        redirect_to articles_path
+    end
+    #Edit article form
+    def edit
+        @article = Article.find(params[:id])
+    end
+    #Submit edit
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+        flash.notice = "Article '#{@article.title}' Updated!"
+        redirect_to article_path(@article)
     end
 end
