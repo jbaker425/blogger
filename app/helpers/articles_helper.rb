2ad3597ce13b7351d57433a884479e92c32540fe
James Baker <jamesbaker425@gmail.com>
2020-07-26 21:30:54 -0400

    clean up

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,8 +1,5 @@
 module ArticlesHelper
-<<<<<<< HEAD
     def article_params
         params.require(:article).permit(:title, :body)
-    end   
-=======
->>>>>>> 4c02f2b3cd35c48751a7f46b54e5849a555e8a91
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  root to: 'articles#index'
-=======
->>>>>>> 4c02f2b3cd35c48751a7f46b54e5849a555e8a91
   resources :articles
   resources :articles do
     resources :comments
